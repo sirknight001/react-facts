2b69f4c28dbc0f72a10d471253a001201df233b0
sirknight001 <109674453+sirknight001@users.noreply.github.com>
2022-09-02 11:50:54 +0800

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function Navbar() {
+    return (
+        <nav>
+            <img src={require('./react-icon-small.png')} alt='react-icon' />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
